db640c79b56d299b7e5e1101e7dcd42959e3b0be
Shane <shanemailer@gmail.com>
2023-03-19 15:29:15 -0500

    feat: add prettier files

---
+++ prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  printWidth: 120,
+  proseWrap: "preserve",
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+};
